71774b64e6cfe1603c5b04b7f1ba8b6c19c04c89
Adam Jaczak <adamjaczak3@gmail.com>
2026-03-31 16:21:55 +0200

    another commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os.path
+from pathlib import Path
 input_data = []
 
 user_choice = 0
+
+trees = []
 
 #sprawdzanie czy większy czy mniejszy i w zależności od tego dodawania na lewo lub prawo? DO ZROBIENIA
 
@@ -10,6 +14,46 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
+
+    def in_order_search(self):
+        if self.left is not None:
+            self.left.in_order_search()
+        print(self.value, end=" ")
+        if self.right is not None:
+            self.right.in_order_search()
+
+    def pre_order_search(self):
+        print(self.value, end=" ")
+        if self.left is not None:
+            self.left.pre_order_search()
+        if self.right is not None:
+            self.right.pre_order_search()
+
+    def save_tree_to_file(self, filepath):
+        with open(filepath, 'w') as file:
+            def write_preorder(current_node):
+                if current_node is None:
+                    return
+                file.write(f"{current_node.value}\n")
+                write_preorder(current_node.left)
+                write_preorder(current_node.right)
+            write_preorder(self)
+
+    def load_tree_from_file(self, filepath):
+
+        if not os.path.exists(filepath):
+            print("Błąd. Plik nie istnieje")
+            return None
+        with open(filepath, 'r') as file:
+            for line in file:
+                value = int(line.strip())
+                if self is None:
+                    self.value = value
+                else:
+                    insert(self, value)
+
+        print("Drzewo zostało zbudowane")
+        return root
 
 
 def insert(node, value):
@@ -30,31 +74,34 @@ def insert(node, value):
     return node
 
 
-def add_values(values: list):
-    central_element = values[len(values) // 2]
-    less_values = values[:central_element]
-    greater_values = values[central_element+1:]
-    root = insert(None, central_element)
+def add_values(values: list, basic_root=None):
+    if not values:
+        return basic_root
+
+    central_element = len(values) // 2
+    print(central_element)
+
+    if basic_root is None:
+        basic_root = Node(values[central_element])
+    else:
+        insert(basic_root, values[central_element])
+
+    add_values(values[:central_element], basic_root)
+    add_values(values[central_element + 1:], basic_root)
+
+    return basic_root
 
 
-def in_order_search(tree_root: Node):
-    if not tree_root:
-        return
-    in_order_search(tree_root.left)
-    print(tree_root.value, end=" ")
-    in_order_search(tree_root.right)
+while user_choice != 4:
+    folder = Path("trees/")
+    if folder.exists() and folder.is_dir():
+        for file in folder.glob("*.txt"):
+            if file.is_file():
+                tree = Node(None)
+                tree.load_tree_from_file(str(file))
+                trees.append(tree)
 
-
-def pre_order_search(tree_root: Node):
-    if not tree_root:
-        return
-    print(tree_root.value, end=" ")
-    pre_order_search(tree_root.left)
-    pre_order_search(tree_root.right)
-
-
-while user_choice != 3:
-    print("\n1. Tworzenie drzewa metodą połowienia binarnego\n2. Tworzenie drzewa metodą dodawania z tablicy\n3. Wyjście")
+    print("\n1. Tworzenie drzewa metodą połowienia binarnego\n2. Tworzenie drzewa metodą dodawania z tablicy\n3.Wypisz drzewa z pliku\n4. Wyjście")
 
     flag = False
 
@@ -70,8 +117,12 @@ while user_choice != 3:
         values = [int(x) for x in input().split(' ')]
         values.sort()
         print(f"Posortowane wartości wejściowe: {values}")
-        root = insert(None, values)
+        root = add_values(values)
         print("In order\n")
-        in_order_search(root)
+        root.in_order_search()
         print("\nPre order\n")
-        pre_order_search(root)
+        root.pre_order_search()
+        root.save_tree_to_file("trees/drzewo1.txt")
+    elif user_choice == 3:
+        for tree in trees:
+            tree.pre_order_search()
